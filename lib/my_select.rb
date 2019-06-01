cf7ad61793d4a1787ec9596b1da11afde464d1bb
Jourdan Hamilton <jourdan.hamilton1@gmail.com>
2019-06-01 00:26:12 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
-def my_select(collection)
- # your code here!
-end
+numslist = [1, 2, 3, 4, 5, 6]
+
+
+def my_select(array)
+  i = 0
+  select = []
+  while i < array.length
+      if yield(array[i])
+        select.push( array[i])
+      end
+      i+=1
+      end
+      select
+    end
+
+    my_select(numslist) { | num | num.even? }
