d8808cb570777afece135b337a2b03c4c9e92b54
Jourdan Hamilton <jourdan.hamilton1@gmail.com>
2019-06-01 00:20:45 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -5,7 +5,8 @@ def my_select(array)
   i = 0
   select = []
   while i < array.length
-      if yield(array[i])
+      if yield
+        (array[i])
         select << array[i]
       end
       i+=1
